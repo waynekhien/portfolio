b7bcf5a1e0ef047334cca9fe130877c6e56d65ba
waynekhien <khien23092003@gmail.com>
2025-08-28 13:39:20 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+
+export const Home = () => {
+    return <div className="min-h-screen">
+        {/* Theme Toggle */}
+        <ThemeToggle />
+        {/* Background Effects */}
+
+        {/* NavBar */}
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+    </div>
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,6 @@
+export const NotFound = () => {
+
+    return <div>
+        Not Found
+    </div>
+}
